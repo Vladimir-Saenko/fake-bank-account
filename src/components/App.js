fa83ef2550530812f6ac9afc94e3cedabea9e2c8
Vladimir <siborg.451@gmail.com>
2024-04-19 09:08:10 +0500

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Fake Bank Account (useReducer)</h1>
+    </header>
+  );
+}
